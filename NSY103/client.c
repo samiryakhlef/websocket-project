00b24701450cd0a44a43cdf91b3a659321b63801
grolier.shagan@gmali.com <grolier.shagan@gmali.com>
2023-06-03 12:03:19 +0200

    thread

---
+++ NSY103/client.c
@@ -0,0 +1,64 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <signal.h>
+
+
+#define BUFFER_LEN 1024
+
+// typedef struct CLIENT{
+//     char NOM[30];
+//     int age;
+//     int id;
+
+// }CLIENT;
+
+int main(void)
+{
+    char buffer[BUFFER_LEN];
+    int socketClient = socket(AF_INET, SOCK_STREAM, 0);
+    struct sockaddr_in addrClient;
+    addrClient.sin_family = AF_INET;
+    addrClient.sin_port = htons(1234);
+    addrClient.sin_addr.s_addr = inet_addr("192.168.80.20");
+
+    // test création socket
+    if (socketClient == -1)
+    {
+        printf("error socket creation\n");
+    }
+
+    // CLIENT Client;
+
+    // connexion du client
+    if (connect(socketClient, (struct sockaddr *)&addrClient, sizeof(addrClient)) == -1)
+    {
+        printf("error Openning a connection on socket FD to peer at ADDR");
+    }
+
+   
+    recv(socketClient, buffer, sizeof(buffer), 0);
+    printf("%s\n", buffer);
+
+    recv(socketClient, buffer, sizeof(buffer), 0);
+    printf("%s", buffer);
+
+    scanf("%s", buffer);
+    send(socketClient, buffer, sizeof(buffer), 0);
+
+    
+    recv(socketClient, buffer, sizeof(buffer), 0);
+    printf("%s", buffer);
+    scanf("%s", buffer);
+    send(socketClient, buffer, sizeof(buffer), 0);
+
+    close(socketClient);
+
+    return 0;
+}
--- NSY103/server.c
+++ NSY103/server.c
@@ -1,10 +1,98 @@
-#include <unistd.h>
-#include <stdio.h>
+#include <sys/types.h>
 #include <sys/socket.h>
-#include <stdlib.h>
 #include <netinet/in.h>
-#include <string.h>
-#include <errno.h>
+#include <unistd.h>
+#include <netdb.h>
 #include <arpa/inet.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <signal.h>
+#include <pthread.h>
+#include <fcntl.h>
 
-vs code c de la merde
+#define BUFFER_LEN 1024
+static char buffer[BUFFER_LEN];
+
+void *function(void *arg)
+{
+    int socket = (int *)arg; //diff
+    char pseudo[35];
+    strcpy(pseudo,"quel est ton pseudo ?\n" );
+    send(socket, pseudo, sizeof(pseudo), 0);
+    
+    recv(socket, buffer, sizeof(buffer), 0);
+    printf("%s est connecté\n", buffer);
+
+    // communiquer avecqui ? 
+    char contact[35];
+    strcpy(contact,"tu veux parler à qui ? \n");
+    send(socket, contact, sizeof(contact), 0);
+    recv(socket, buffer, sizeof(buffer), 0);
+    printf("demande de connexion pour : %s \n", buffer);
+
+    close(socket);
+    pthread_exit(NULL);
+}
+
+int main(void)
+{
+   
+
+    pthread_t clientThread;
+    
+    int socketServer = socket(AF_INET, SOCK_STREAM, 0);
+    struct sockaddr_in addrServer;
+    addrServer.sin_family = AF_INET;
+    addrServer.sin_port = htons(1234);
+    addrServer.sin_addr.s_addr = inet_addr("192.168.80.20");
+    
+    // test création socket
+    if (socketServer == -1) {
+        printf("error socket creation\n");
+        return 1;
+    }
+    
+    // donne au fd l'adresse locale
+    if (bind(socketServer, (const struct sockaddr *)&addrServer, sizeof(addrServer))) {
+        printf("error Giving the socket FD the local address ADDR\n");
+        close(socketServer);
+        return 1;
+    }
+    
+    // prepare pour accepter connexion
+    // fd, nb de personne en attente
+    if (listen(socketServer, 10) == -1) {
+        printf("error Preparing to accept connections on socket FD\n");
+        close(socketServer);
+        return 1;
+    }
+    
+    printf("listen\n");
+    
+    pthread_t threadClient[3];
+    
+    for (int i = 0; i < 3; i++) {
+        struct sockaddr_in addrClient;
+        socklen_t csize = sizeof(addrClient);
+        
+        int socketClient = accept(socketServer, (struct sockaddr *)&addrClient, &csize);
+        printf("accept\n");
+        
+        //char message[100] = "connecté sur le serveur ROCKY";
+        //send(socketClient, message, sizeof(message), 0);
+        char message[35];
+        strcpy(message, "connecté sur le serveur ROCKY");
+        send(socketClient, message, sizeof(message), 0);
+        
+        pthread_create(&threadClient[i], NULL, function, (void *)socketClient);
+    }
+    
+    for (int i = 0; i < 3; i++) {
+        pthread_join(threadClient[i], NULL);
+    }
+    
+    close(socketServer);
+    printf("close\n");
+    return 0;
+}
