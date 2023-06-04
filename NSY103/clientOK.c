0a71fbbcd2be58e5dc6f305db324585f73837ae9
SG <grolier.shagan@gmali.com>
2023-06-05 01:52:11 +0200

    server/client last

---
+++ NSY103/clientOK.c
@@ -0,0 +1,98 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <pthread.h>
+
+#define SERVER_IP "192.168.80.20"
+#define PORT 1234
+char buffer[1024];
+    int read_size;
+
+void *receive_messages(void *arg)
+{
+    int client_socket = *(int *)arg;
+    
+
+    while ((read_size = recv(client_socket, buffer, sizeof(buffer), 0)) > 0)
+    {
+        buffer[read_size] = '\0';
+        printf("Received message: %s\n", buffer);
+
+    }
+    
+
+    if (read_size == 0)
+    {
+        printf("Disconnected from server\n");
+    }
+    else if (read_size == -1)
+    {
+        perror("recv failed\n");
+    }
+
+    pthread_exit(NULL);
+}
+
+void start_client()
+{
+    int client_socket;
+    struct sockaddr_in server_addr;
+    char message[1024];
+    pthread_t recv_thread;
+
+    client_socket = socket(AF_INET, SOCK_STREAM, 0);
+    if (client_socket == -1)
+    {
+        perror("socket creation failed\n");
+        exit(EXIT_FAILURE);
+    }
+
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(PORT);
+    if (inet_pton(AF_INET, SERVER_IP, &(server_addr.sin_addr)) <= 0)
+    {
+        perror("invalid address / address not supported\n");
+        exit(EXIT_FAILURE);
+    }
+
+    if (connect(client_socket, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1)
+    {
+        perror("connection failed\n");
+        exit(EXIT_FAILURE);
+    }
+     
+     
+    if (pthread_create(&recv_thread, NULL, receive_messages, (void *)&client_socket) != 0)
+    {
+        perror("thread creation failed\n");
+        exit(EXIT_FAILURE);
+    }
+
+   
+
+    while (1)
+    {
+        printf("Enter a message (or 'exit' to quit): \n");
+        fgets(message, sizeof(message), stdin);
+        message[strcspn(message, "\n")] = '\0';
+
+        if (strcmp(message, "exit") == 0)
+        {
+            break;
+        }
+
+        send(client_socket, message, strlen(message), 0);
+    }
+
+    close(client_socket);
+    pthread_cancel(recv_thread);
+    pthread_join(recv_thread, NULL);
+}
+
+int main()
+{
+    start_client();
+    return 0;
+}
---
+++ NSY103/newServer.c
@@ -0,0 +1,195 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <signal.h>
+#include <pthread.h>
+#include <fcntl.h>
+
+#define BUFFER_LEN 1024
+static char buffer[BUFFER_LEN];
+
+typedef struct
+{
+    int socket;
+    char name[35];
+} Client;
+
+typedef struct
+{
+    int maxClients;
+    Client *clientList;
+} ServerData;
+
+void *function(void *arg)
+{
+    ServerData *serverData = (ServerData *)arg;
+    Client *client = &(serverData->clientList[serverData->maxClients - 1]);
+
+    char contact[35];
+    strcpy(contact, "tu veux parler à qui ? \n");
+    if (send(client->socket, contact, sizeof(contact), 0) == -1)
+    {
+        perror("Error sending contact question");
+        close(client->socket);
+        pthread_exit(NULL);
+    }
+
+    if (recv(client->socket, buffer, sizeof(buffer), 0) <= 0)
+    {
+        perror("Error receiving contact");
+        close(client->socket);
+        pthread_exit(NULL);
+    }
+    printf("%s veut communiquer avec : %s \n", client->name, buffer);
+
+    // Communication entre les clients
+    // Trouver le client cible avec lequel communiquer
+    int targetClientIndex = -1;
+    for (int i = 0; i < serverData->maxClients; i++)
+    {
+        if (strcasecmp(buffer, serverData->clientList[i].name) == 0)
+        {
+            targetClientIndex = i;
+            break;
+        }
+    }
+
+    if (targetClientIndex == -1)
+    {
+        printf("Target client not found.\n");
+        close(client->socket);
+        pthread_exit(NULL);
+    }
+
+   // Écoute continuellement les messages du client et les envoie au client cible.
+    while (1)
+    {
+        // Réception d'un message du client
+        int recvSize = recv(client->socket, buffer, sizeof(buffer), 0);
+        if (recvSize <= 0)
+        {
+            perror("Error receiving message");
+            break;
+        }
+
+     // Mettre fin à la communication si un signal de fin de communication est reçu
+        if (strcmp(buffer, "quit") == 0)
+        {
+            printf("%s has terminated the communication.\n", client->name);
+            break;
+        }
+
+       // Envoi d'un message au client
+        int targetSocket = serverData->clientList[targetClientIndex].socket;
+        if (targetSocket == -1)
+        {
+            printf("Invalid socket descriptor for target client.\n");
+            break;
+        }
+
+        if (send(targetSocket, buffer, recvSize, 0) == -1)
+        {
+            perror("Error sending message");
+            break;
+        }
+    }
+
+    close(client->socket);
+    pthread_exit(NULL);
+}
+
+int main(void)
+{
+    int socketServer = socket(AF_INET, SOCK_STREAM, 0);
+    struct sockaddr_in addrServer;
+    addrServer.sin_family = AF_INET;
+    addrServer.sin_port = htons(1234);
+    addrServer.sin_addr.s_addr = inet_addr("192.168.80.20");
+
+    // test création socket
+    if (socketServer == -1)
+    {
+        printf("error socket creation\n");
+        return 1;
+    }
+
+    // donne au fd l'adresse locale
+    if (bind(socketServer, (const struct sockaddr *)&addrServer, sizeof(addrServer)))
+    {
+        printf("error Giving the socket FD the local address ADDR\n");
+        close(socketServer);
+        return 1;
+    }
+
+    // prepare pour accepter connexion
+    // fd, nb de personne en attente
+    if (listen(socketServer, 10) == -1)
+    {
+        printf("error Preparing to accept connections on socket FD\n");
+        close(socketServer);
+        return 1;
+    }
+
+    printf("listen\n");
+
+    int maxClients = 3; // Maximum number of clients
+    Client *clientList = malloc(maxClients * sizeof(Client));
+
+    for (int i = 0; i < maxClients; i++)
+    {
+        struct sockaddr_in addrClient;
+        socklen_t csize = sizeof(addrClient);
+
+        int socketClient = accept(socketServer, (struct sockaddr *)&addrClient, &csize);
+        printf("accept\n");
+
+        char message[35];
+        strcpy(message, "connecté sur le serveur\n"); // %s supprimé
+        strcat(message, " Quel est ton pseudo ? \n" );
+        if (send(socketClient, message, strlen(message) + 1, 0) == -1)
+        {
+            perror("Error sending connection message\n");
+            close(socketClient);
+            continue;
+        }
+
+        // Reception du nom du client
+        if (recv(socketClient, buffer, sizeof(buffer), 0) <= 0)
+        {
+            perror("Error receiving client name\n");
+            close(socketClient);
+            continue;
+        }
+
+        printf("%s est connecté\n", buffer);
+
+        Client newClient;
+        newClient.socket = socketClient;
+        strcpy(newClient.name, buffer);
+
+        clientList[i] = newClient;
+
+        pthread_t clientThread;
+        ServerData serverData;
+        serverData.maxClients = i + 1;
+        serverData.clientList = clientList;
+
+        if (pthread_create(&clientThread, NULL, function, (void *)&serverData) != 0)
+        {
+            perror("Error creating client thread\n");
+            close(socketClient);
+            continue;
+        }
+    }
+
+    free(clientList);
+    close(socketServer);
+    printf("close\n");
+    return 0;
+}
